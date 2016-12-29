7607b75d58e01ef065d5eaf789c681b49404c90f
Patrick José Pereira <patrickelectric@gmail.com>
2016-12-29 10:02:02 -0200

    Add owner to user
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- socat.sh
+++ socat.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-uid=`id -u $USERNAME`
+if [ "$#" -ne 1 ]; then
+    echo "Use: sudo ./socat.sh youruser"
+fi
+
 if [ "$USER" = "root" ]
 then
-	USER2=$SUDO_USER
 	socat PTY,link=/dev/ttyVirtual1 PTY,link=/dev/ttyVirtual2&
 	sleep 1
-	chown $USER2:uucp /dev/ttyVirtual1
-	chown $USER2:uucp /dev/ttyVirtual2
-	echo "Created ports, set ownership to user $USER2"
+	chown $1:uucp /dev/ttyVirtual1
+	chown $1:uucp /dev/ttyVirtual2
+	echo "Created ports, set ownership to user $1"
 	echo "Connect to /dev/ttyVirtual1 and /dev/ttyVirtual2"
 else
 	echo "RUN WITH SUDO!"
